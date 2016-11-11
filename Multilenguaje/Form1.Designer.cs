4dee5e50c3c8fe44491be3ba4986c74e1079ac70
Nelson Venegas <nvenegar@hotmail.com>
2016-11-10 20:44:40 -0500

    Actualizacion de recursos de Idioma

--- Multilenguaje/Form1.Designer.cs
+++ Multilenguaje/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Multilenguaje
 {
-    partial class Form1
+    partial class frm1Main
     {
         /// <summary>
         /// Variable del diseñador necesaria.
@@ -28,12 +28,116 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.archivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.abrirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cerrarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.guardarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.idiomaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.inglesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.españolToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.archivoToolStripMenuItem,
+            this.idiomaToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(661, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // archivoToolStripMenuItem
+            // 
+            this.archivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.abrirToolStripMenuItem,
+            this.cerrarToolStripMenuItem,
+            this.guardarToolStripMenuItem,
+            this.salirToolStripMenuItem});
+            this.archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
+            this.archivoToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.archivoToolStripMenuItem.Text = "Archivo";
+            // 
+            // abrirToolStripMenuItem
+            // 
+            this.abrirToolStripMenuItem.Name = "abrirToolStripMenuItem";
+            this.abrirToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
+            this.abrirToolStripMenuItem.Text = "Abrir";
+            // 
+            // cerrarToolStripMenuItem
+            // 
+            this.cerrarToolStripMenuItem.Name = "cerrarToolStripMenuItem";
+            this.cerrarToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
+            this.cerrarToolStripMenuItem.Text = "Cerrar";
+            // 
+            // guardarToolStripMenuItem
+            // 
+            this.guardarToolStripMenuItem.Name = "guardarToolStripMenuItem";
+            this.guardarToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
+            this.guardarToolStripMenuItem.Text = "Guardar";
+            // 
+            // salirToolStripMenuItem
+            // 
+            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
+            this.salirToolStripMenuItem.Text = "Salir";
+            // 
+            // idiomaToolStripMenuItem
+            // 
+            this.idiomaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.inglesToolStripMenuItem,
+            this.españolToolStripMenuItem});
+            this.idiomaToolStripMenuItem.Name = "idiomaToolStripMenuItem";
+            this.idiomaToolStripMenuItem.Size = new System.Drawing.Size(56, 20);
+            this.idiomaToolStripMenuItem.Text = "Idioma";
+            // 
+            // inglesToolStripMenuItem
+            // 
+            this.inglesToolStripMenuItem.Name = "inglesToolStripMenuItem";
+            this.inglesToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.inglesToolStripMenuItem.Text = "Ingles";
+            this.inglesToolStripMenuItem.Click += new System.EventHandler(this.inglesToolStripMenuItem_Click);
+            // 
+            // españolToolStripMenuItem
+            // 
+            this.españolToolStripMenuItem.Name = "españolToolStripMenuItem";
+            this.españolToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.españolToolStripMenuItem.Text = "Español";
+            this.españolToolStripMenuItem.Click += new System.EventHandler(this.españolToolStripMenuItem_Click);
+            // 
+            // frm1Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(661, 383);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frm1Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Aplicacion Multilenguaje ";
+            this.Load += new System.EventHandler(this.frm1Main_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem archivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem abrirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cerrarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem guardarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem idiomaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem inglesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem españolToolStripMenuItem;
     }
 }
 
--- Multilenguaje/Form1.cs
+++ Multilenguaje/Form1.cs
@@ -7,14 +7,89 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Resources;
+using System.Globalization;
+
+
+
+
 
 namespace Multilenguaje
 {
-    public partial class Form1 : Form
+   
+    public partial class frm1Main : Form
     {
-        public Form1()
+        // Declara variable ResManager para acceder a Resouce Manager. Para administrar recursos que proporcionan un acceso mas comodo a los recursos de la referencia cultural
+        ResourceManager ResManager;
+        // Declara variable para acceder a a una referencia cultural ( Configuracion Regional )
+        CultureInfo CuInfo;
+        public frm1Main()
         {
             InitializeComponent();
         }
-    }
+
+        void switch_language()
+        {
+            if (españolToolStripMenuItem .Checked == true)    //En español
+            {
+                CuInfo = CultureInfo.CreateSpecificCulture("ES-CO");    //crea culture info para español
+            }
+            else                                                //En English
+            {
+                CuInfo = CultureInfo.CreateSpecificCulture("EN-US");     //create culture for english
+            }
+
+            this.abrirToolStripMenuItem.Text = ResManager.GetString("abrirToolStripMenuItem", CuInfo);
+            this.guardarToolStripMenuItem.Text= ResManager.GetString("guardarToolStripMenuItem", CuInfo);
+            this.cerrarToolStripMenuItem.Text = ResManager.GetString("cerrarToolStripMenuItem", CuInfo);
+            this.salirToolStripMenuItem.Text = ResManager.GetString("salirToolStripMenuItem", CuInfo);
+            this.inglesToolStripMenuItem.Text = ResManager.GetString("inglesToolStripMenuItem", CuInfo);
+            this.españolToolStripMenuItem.Text = ResManager.GetString("españolToolStripMenuItem", CuInfo);
+        }
+
+        private void frm1Main_Load(object sender, EventArgs e)
+        {
+            españolToolStripMenuItem .Checked = false;    //default language is english
+            inglesToolStripMenuItem .Checked = true;  //Defecto Idioma Ingles
+            ResManager = new ResourceManager("Multilenguaje.RecursosdeIdioma.Res", typeof(frm1Main).Assembly);
+            //switch to vietnamese
+            switch_language();
+
+            this.abrirToolStripMenuItem.Text = ResManager.GetString("abrirToolStripMenuItem", CuInfo);       
+        }
+
+        private void inglesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (inglesToolStripMenuItem.Checked == true)    //in Ingles, switch to default language
+            {
+                españolToolStripMenuItem.Checked = false;
+                inglesToolStripMenuItem.Checked = true;        //default language
+            }
+            else            //current language is not french, switch french
+            {
+                inglesToolStripMenuItem.Checked = true;
+                españolToolStripMenuItem.Checked = false;                
+            }
+            //switch language
+            switch_language();
+        }
+
+        private void españolToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (españolToolStripMenuItem.Checked == true)    //in Ingles, switch to default language
+            {
+                españolToolStripMenuItem.Checked = true ;
+                inglesToolStripMenuItem.Checked = false;        //default language
+            }
+            else            //current language is not french, switch french
+            {
+                inglesToolStripMenuItem.Checked = true;
+                españolToolStripMenuItem.Checked = false;
+            }
+            //switch language
+            switch_language();
+        }
+    }  
+
 }
+
